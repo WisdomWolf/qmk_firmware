e9700ec1d33321c26b46848636817b6e3eaab082
WisdomWolf <wisdomwolf@gmail.com>
2020-05-13 20:47:35 -0400

    Adjusts custom layout for theta keyboard

--- keyboards/theta/keymaps/wisdomwolf/keymap.c
+++ keyboards/theta/keymaps/wisdomwolf/keymap.c
@@ -12,7 +12,8 @@ enum custom_layers
 	_QWERTY,
 	_LOWER,
 	_RAISE,
-	_ADJUST
+	_ADJUST,
+  _NUMPAD
 };
 
 enum custom_keycodes
@@ -20,7 +21,8 @@ enum custom_keycodes
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST
+  ADJUST,
+  NUMPAD
 };
 
 // Fillers to make layering more clear
@@ -30,35 +32,41 @@ enum custom_keycodes
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_ortho_4x12( \
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_QUOTE), \
-  _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   ADJUST, KC_DOWN,    KC_UP, _______  \
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,     KC_BSPC, \
+  KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,      KC_SCLN,  KC_ENT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH,  RSFT_T(KC_QUOTE), \
+  XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_RIGHT,  KC_UP,    XXXXXXX  \
 ),
 
 
 [_LOWER] = LAYOUT_ortho_4x12( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  BL_STEP, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, _______ \
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,   KC_AMPR,     KC_ASTR,     KC_LPRN,  KC_RPRN, KC_BSPC, \
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT,   KC_DOWN,     KC_UP,       KC_RIGHT, KC_PIPE, KC_PIPE, \
+  BL_STEP, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    S(KC_NUHS),  S(KC_NUBS),  _______,  _______, _______, \
+  XXXXXXX, NUMPAD, _______, _______, _______, _______, _______,   ADJUST,      KC_VOLD,     KC_VOLU,  KC_MPLY, XXXXXXX \
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12( \
-  KC_GRV,     KC_1,   KC_2,      KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_DEL,  KC_MS_L,   KC_MS_U,  KC_MS_R,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_MS_L,   KC_MS_D, KC_MS_R, KC_BTN1,  KC_BTN2,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______, KC_BTN1,   KC_BTN2,  _______, _______, _______, _______, _______, _______ \
+  KC_GRV,     KC_1,     KC_2,      KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+  KC_TAB,     KC_MS_L,  KC_MS_U,   KC_MS_R,  KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______,    KC_MS_L,  KC_MS_D,   KC_MS_R,  KC_BTN1, KC_BTN2, KC_F12,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
+  XXXXXXX,    _______,  _______,   _______,  ADJUST,  KC_BTN2, _______, _______, _______, KC_LPRN, KC_RPRN, XXXXXXX \
 ),
 
 
 [_ADJUST] =  LAYOUT_ortho_4x12( \
-  KC_F1,     KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  _______, _______,   KC_UP,    AU_ON,   AU_OFF,  _______, _______, _______, _______, _______, _______,  KC_DEL, \
-  _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, RESET  , _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______ \
-)
+  KC_F1,     KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,   KC_F7,    KC_F8,               KC_F9,               KC_F10,              KC_F11,   KC_F12, \
+  _______,   _______, KC_UP,    AU_ON,    AU_OFF,  _______, _______,  KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, _______,  KC_DEL, \
+  _______,   KC_LEFT, KC_DOWN,  KC_RIGHT, _______, _______, _______,  _______,             _______,             _______,             _______,  _______, \
+  XXXXXXX,   RESET  , _______,  _______,  _______, _______, _______,  _______,             _______,             _______,             _______,  XXXXXXX \
+),
 
+[_NUMPAD] = LAYOUT_ortho_4x12( \
+  KC_GRV,     KC_1,     KC_2,      KC_3,     KC_4,    KC_5,    KC_6,    KC_KP_7,    KC_KP_8,    KC_KP_9,    KC_KP_ASTERISK,  KC_BSPC, \
+  KC_TAB,     KC_MS_L,  KC_MS_U,   KC_MS_R,  KC_F4,   KC_F5,   KC_F6,   KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_KP_MINUS,     KC_KP_SLASH, \
+  _______,    KC_MS_L,  KC_MS_D,   KC_MS_R,  KC_BTN1, KC_BTN2, KC_F12,  KC_KP_1,    KC_KP_2,    KC_KP_3,    KC_KP_PLUS,      KC_EQUAL, \
+  XXXXXXX,    _______,  _______,   _______,  ADJUST,  KC_BTN2, KC_NO,   KC_KP_0,    KC_KP_0,    KC_KP_DOT,  KC_KP_ENTER,     XXXXXXX \
+)
 
 };
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
